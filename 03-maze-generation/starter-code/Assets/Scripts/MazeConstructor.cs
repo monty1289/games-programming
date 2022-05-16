f78c2e2d4fd0c6c26336042c119db9bab68bbc4d
monty1289 <64822697+monty1289@users.noreply.github.com>
2022-05-16 18:29:06 +1200

    lecture notes one up to date

--- 03-maze-generation/starter-code/Assets/Scripts/MazeConstructor.cs
+++ 03-maze-generation/starter-code/Assets/Scripts/MazeConstructor.cs
@@ -7,6 +7,8 @@ public class MazeConstructor : MonoBehaviour
     [SerializeField] private Material mazeMat1;
     [SerializeField] private Material mazeMat2;    
     [SerializeField] private Material treasureMat;
+    private MazeMeshGenerator meshGenerator;
+    public float placementThreshold = 0.1f;   // chance of empty space
 
     public int[,] data
     {
@@ -22,6 +24,25 @@ public class MazeConstructor : MonoBehaviour
             {1, 0, 1},
             {1, 1, 1}
         };
+        meshGenerator = new MazeMeshGenerator();
+        
+    }
+
+    private void DisplayMaze()
+    {
+        GameObject go = new GameObject();
+        go.transform.position = Vector3.zero;
+        go.name = "Procedural Maze";
+        go.tag = "Generated";
+
+        MeshFilter mf = go.AddComponent<MeshFilter>();
+        mf.mesh = meshGenerator.FromData(data);
+        
+        MeshCollider mc = go.AddComponent<MeshCollider>();
+        mc.sharedMesh = mf.mesh;
+
+        MeshRenderer mr = go.AddComponent<MeshRenderer>();
+        mr.materials = new Material[2] {mazeMat1, mazeMat2};
     }
 
     void OnGUI()
@@ -50,12 +71,29 @@ public class MazeConstructor : MonoBehaviour
             Debug.LogError("Odd numbers work better for dungeon size.");
 
         data = FromDimensions(sizeRows, sizeCols);
+        DisplayMaze();
     }
 
     public int[,] FromDimensions(int sizeRows, int sizeCols)
     {
         int[,] maze = new int[sizeRows, sizeCols];
-        // stub to fill in
+
+        int rMax = maze.GetUpperBound(0);
+        int cMax = maze.GetUpperBound(1);
+
+        for (int i = 0; i <= rMax; i++)        
+            for (int j = 0; j <= cMax; j++)            
+                if (i == 0 || j == 0 || i == rMax || j == cMax)                
+                    maze[i, j] = 1;                                    
+                else if (i % 2 == 0 && j % 2 == 0 && Random.value > placementThreshold)                                    
+                {
+                    maze[i, j] = 1;
+
+                    int a = Random.value < .5 ? 0 : (Random.value < .5 ? -1 : 1);
+                    int b = a != 0 ? 0 : (Random.value < .5 ? -1 : 1);
+                    maze[i+a, j+b] = 1;
+                } 
+
         return maze;
     }
 }
